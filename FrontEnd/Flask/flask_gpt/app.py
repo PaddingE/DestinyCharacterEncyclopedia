85c22b8fb42208c3463a750f2086bc66bbd3b3ed
PaddingE <voelddl95@naver.com>
2023-08-21 21:23:36 +0900

    we

---
+++ FrontEnd/Flask/flask_gpt/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+import subclass as sc
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return  render_template('Home.html')
+
+@app.route("/output", methods = ['GET'])
+def view():
+    
+    input_text = request.args['input_text']
+    
+    pp = sc.Preprocessing()
+    output = sc.Subclass()
+
+    
+    item_list_data, name, df_text = pp.search_character(input_text)
+    character_name, summary_text, translate_text = pp.find_data(input_text)
+    list_output_list = output.make_output(df_text, name)
+    
+    text_output = ''
+    
+    for text in list_output_list:
+        text_output += text
+        
+    return render_template('answer.html', output_text = translate_text, character_text = character_name,summary_text = text_output,
+                           table = item_list_data)
+
+@app.route("/reference/<item>")
+def click_reference(item):
+    
+    pp = sc.Preprocessing()
+    
+    title,description,season = pp.find_item(item)
+    
+    return render_template('item.html', item_title = title, item_description = description, item_season = season)
+
+@app.route("/Humanity")
+def humanity():
+    return render_template('Humanity.html')
+    
+if __name__ == "__main__":
+    app.run()
